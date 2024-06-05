7355b898099567d9315e5fac81793d166d87267d
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-06-05 03:38:30 -0700

    3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: The tree to delete
+ *
+ * Return: Nothing
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	binary_tree_t *temp_left, *temp_right;
+
+	if (tree == NULL)
+		return;
+
+	temp_left = tree->left;
+	temp_right = tree->right;
+	free(tree);
+	binary_tree_delete(temp_left);
+	binary_tree_delete(temp_right);
+}

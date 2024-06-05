b81e609927d86dfb356d7e7c4b1f039f5a9d4b22
damaris-lab <damarisnyawiragacheru@gmail.com>
2024-06-05 04:18:26 -0700

    12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,17 @@
+
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: A pointer to the root node of the tree to count
+ * the number of leaves
+ *
+ * Return: The number of leaves in the binary tree
+ * If tree is NULL return 0
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else if (tree->right == NULL && tree->left == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
